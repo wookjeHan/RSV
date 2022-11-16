aa5f83de08f0f1c453ca47b4a44827abc7002ecf
mountguy <riman314@snu.ac.kr>
2022-11-17 04:07:10 +0900

    global config file

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
+from dataclasses import dataclass
+
 # Global Configs
+@dataclass
 class GlobalConfig:
-    language_model ='gpt2'
-    dataset ='superglue_cb'
-    batch_size =4
-    shot_num =3
-    tv_split_ratio =0.7
+    language_model = 'gpt2'
+    dataset = 'superglue_cb'
+    batch_size = 2
+    shot_num = 3
+    tv_split_ratio = 0.7
