masking bug fix

rl/envs/classification_env.py
@@ -8,7 +8,7 @@ class ClassificationEnv():
     '''
     Classification Env which can manage multiple rollout
     '''
-    def __init__(self, trainset, resolver, shot_num, language_model, tokenizer, verbalizers, correct_coeff, incorrect_coeff):
+    def __init__(self, trainset, resolver, shot_num, endo_train, language_model, tokenizer, verbalizers, correct_coeff, incorrect_coeff):
         self.trainset = trainset
         self.resolver = resolver
         self.shot_num = shot_num
@@ -18,11 +18,12 @@ class ClassificationEnv():
         self.class_num = len(verbalizers)
         self.correct_coeff = correct_coeff
         self.incorrect_coeff = incorrect_coeff
+        self.endo_train = endo_train
 
         # TODO: fix it to recieve an embedder as a parameter
         self.embedder = SentenceTransformer('stsb-roberta-large')
 
-    def reset(self, resolved_batch, endo_train, mode):
+    def reset(self, resolved_batch, mode):
         '''
         mode: 'train' | 'test'
         train: env is called during training, reward is calculated
@@ -42,7 +43,7 @@ class ClassificationEnv():
         batch_size = len(resolved_inputs)
         action_mask = torch.ones(batch_size, len(self.trainset), device=get_device())
 
-        if endo_train:
+        if self.endo_train:
             action_mask[range(batch_size), indices] = 0
 
         self.state = (0, resolved_inputs, action_mask)

rl/sampler.py
@@ -5,36 +5,36 @@ from rl.envs.classification_env import ClassificationEnv
 def sample(policy: MlpPolicy, target_policy: MlpPolicy, env: ClassificationEnv, resolved_batch, shot_num):
     logit_stack = []
     target_logit_stack = []
-    action_stack = []
+    indices_stack = []
 
     target_policy.eval()
 
     states = env.reset(resolved_batch, 'train')
     for step in range(shot_num):
-        actions, logits = policy.get_actions_logits(states)
+        (indices, replace), logits = policy.get_actions_logits(states)
         target_logits = target_policy.get_logits(states)
-        states, rewards = env.step(actions)
+        states, rewards = env.step((indices, replace))
         logit_stack.append(logits.unsqueeze(1))
         target_logit_stack.append(target_logits.unsqueeze(1))
-        action_stack.append(actions.unsqueeze(1))
+        indices_stack.append(indices.unsqueeze(1))
 
     total_logits = torch.cat(logit_stack, dim=1)
     total_target_logits = torch.cat(target_logit_stack, dim=1).detach()
-    total_actions = torch.cat(action_stack, dim=1)
+    total_indices = torch.cat(indices_stack, dim=1)
 
-    return total_logits, total_target_logits, total_actions, rewards
+    return total_logits, total_target_logits, total_indices, rewards
 
 def argmax(policy: MlpPolicy, target_policy: MlpPolicy, env: ClassificationEnv, resolved_batch, shot_num):
-    action_stack = []
+    indices_stack = []
 
     target_policy.eval()
 
     states = env.reset(resolved_batch, 'train')
     for step in range(shot_num):
-        actions = policy.get_actions(states, max_action=True)
-        states, rewards = env.step(actions)
-        action_stack.append(actions.unsqueeze(1))
+        (indices, action_mask) = policy.get_actions(states, max_action=True)
+        states, rewards = env.step((indices, action_mask))
+        indices_stack.append(indices.unsqueeze(1))
 
-    total_actions = torch.cat(action_stack, dim=1)
+    total_indices = torch.cat(indices_stack, dim=1)
 
-    return total_actions, rewards
+    return total_indices, rewards

util/etc.py
@@ -41,9 +41,16 @@ def fix_seed(seed):
     torch.cuda.manual_seed_all(seed)
 
 def get_exp_name(args, trim=False):
-    exp_name = f"sn{args.shot_num}_k{args.topk}_t{args.temperature}"
+    exp_name = f"sn{args.shot_num}_k{args.topk}_lr{args.lr}"
+
     if args.weight_decay > 0.0:
         exp_name += f"_wd{args.weight_decay}"
+
+    if args.replace:
+        exp_name += f"_rep"
+
+    if args.tv_split_ratio == 0.0:
+        exp_name += f"_endo"
 
     if trim:
         return exp_name
@@ -53,7 +60,7 @@ def get_exp_name(args, trim=False):
             f"{args.dataset}_sr{args.tv_split_ratio}",
             args.prompt,
             exp_name
-        ) 
+        )
 
 class Color:
     PURPLE = '\033[95m'