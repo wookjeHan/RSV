e5ed04be7eefd6fb4d4550b1e7b26933cd166af5
mountguy <riman314@snu.ac.kr>
2022-11-22 14:48:13 +0900

    use truncator on test

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ import torch
 
 from transformers import AutoTokenizer
 from language_model import ClassificationModel
-from datasets import load_dataset
 
 from rl.policies.mlp_policy import MlpPolicy
 from rl.envs.classification_env import ClassificationEnv
 
 from util.dataset import DataModule, get_splited_dataset
-from util.nlp import composite, get_input_parameters
+from util.nlp import get_input_parameters
+from util.truncator import Truncator
 from util.etc import fix_seed, get_exp_name, set_device, get_device
 
 import resolvers
@@ -18,7 +18,7 @@ from config import GlobalConfig, TestSnapshotIndex as tsi
 
 
 @torch.no_grad()
-def test(language_model, tokenizer, test_dataloader, shot_selector):
+def test(language_model, tokenizer, test_dataloader, shot_selector, truncator):
     predictions = []
     labels = []
 
@@ -33,7 +33,7 @@ def test(language_model, tokenizer, test_dataloader, shot_selector):
         shots = shot_selector(resolved_batch)
 
         # Construct the list of strings based on the components of batch and shots
-        inputs = composite(resolved_batch, shots)
+        inputs = truncator.truncate(shots, resolved_batch)
 
         # get tokenized ids and attention masks
         verb_input_ids, verb_att_mask, loss_att_mask = get_input_parameters(
@@ -77,33 +77,41 @@ def main(args):
     tokenizer = AutoTokenizer.from_pretrained(args.language_model)
     tokenizer.pad_token = tokenizer.eos_token
 
+    # Truncator
+    truncator = Truncator(tokenizer, 1024, 256)
+
     M = len(shot_selector_trainset)
     policy = MlpPolicy(1024, [2 * M, 2 * M], M, tsi.replace).cuda()
     exp_name = get_exp_name(tsi)
-    env = ClassificationEnv(
-        shot_selector_trainset,
-        resolver,
-        args.shot_num,
-        tsi.tv_split_ratio == 0.0,
-        language_model,
-        tokenizer,
-        verbalizers,
-        200.0,
-        180.0,
-    )
 
-    for shot_selector_func in [shot_selectors.ours, shot_selectors.random, shot_selectors.closest]:
-        print(shot_selector_func)
-        for seed in range(4):
-            if shot_selector_func == shot_selectors.ours:
-                tsi.seed = seed
-                snapshot_path = f"result/{exp_name}/{tsi.seed}/itr_{tsi.epoch}.pt"
-                state_dict = torch.load(snapshot_path, map_location=get_device())
-                policy.load_state_dict(state_dict)
-            shot_selector = shot_selector_func(trainset, args.shot_num, resolver=resolver, policy=policy, env=env)
-            acc = test(language_model, tokenizer, test_dataloader, shot_selector)
-            print(f"[{seed:1d}/4] Accuracy {acc:.3f}")
-            del shot_selector
+    for shot_num in [12, 16]:
+        print(f">>>shot num {shot_num}>>>")
+        env = ClassificationEnv(
+            shot_selector_trainset,
+            resolver,
+            shot_num,
+            tsi.tv_split_ratio == 0.0,
+            language_model,
+            tokenizer,
+            verbalizers,
+            200.0,
+            180.0,
+        )
+
+        for shot_selector_func in [shot_selectors.ours, shot_selectors.random, shot_selectors.closest]:
+            print(shot_selector_func)
+            for seed in range(4):
+                if shot_selector_func == shot_selectors.ours:
+                    tsi.seed = seed
+                    snapshot_path = f"result/{exp_name}/{tsi.seed}/itr_{tsi.epoch}.pt"
+                    state_dict = torch.load(snapshot_path, map_location=get_device())
+                    policy.load_state_dict(state_dict)
+                    shot_selector = shot_selector_func(shot_selector_trainset, shot_num, resolver=resolver, policy=policy, env=env)
+                else:
+                    shot_selector = shot_selector_func(trainset, shot_num, resolver=resolver, policy=policy, env=env)
+                acc = test(language_model, tokenizer, test_dataloader, shot_selector, truncator)
+                print(f"[{seed:1d}/4] Accuracy {acc:.3f}")
+                del shot_selector
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -114,7 +122,6 @@ if __name__ == '__main__':
     parser.add_argument('--prompt', type=str, default='manual')
 
     parser.add_argument('--batch_size', type=int, default=GlobalConfig.batch_size)
-    parser.add_argument('--shot_num', type=int, default=GlobalConfig.shot_num)
     args = parser.parse_args()
 
     main(args)
