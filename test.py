f79e8965961ccd41f1cf44a35414d6645f79cb6d
mountguy <riman314@snu.ac.kr>
2022-12-15 17:44:01 +0900

    make shot selector selection as a argument

--- test.py
+++ test.py
@@ -87,7 +87,6 @@ def main(args):
     M = len(trainset)
     policy = MlpPolicy(1024, [M, M], M, True).cuda()
 
-    print(f">>>shot num {args.shot_num}>>>")
     env = ClassificationEnv(
         trainset,
         True,
@@ -100,35 +99,35 @@ def main(args):
         200.0,
         180.0,
     )
+    shot_selector_func = getattr(shot_selectors, args.shot_selector)
 
-    for shot_selector_func in [shot_selectors.ours]:
-        print(shot_selector_func)
-        if shot_selector_func == shot_selectors.ours:
-            for seed in range(4):
-                snapshot_path = f"result/OURS/{args.dataset}_sr0.0/{args.prompt}/sn{args.shot_num}_k5_lr0.001_bs4_inner/{seed}/itr_90.pt"
-                state_dict = torch.load(snapshot_path, map_location=get_device())
-                policy.load_state_dict(state_dict)
+    print(f"Evaluating the performace of {args.shot_num} shot {args.shot_selector}")
+    if shot_selector_func == shot_selectors.ours:
+        for seed in range(4):
+            snapshot_path = f"result/OURS/{args.dataset}_sr0.0/{args.prompt}/sn{args.shot_num}_k5_lr0.001_bs4_inner/{seed}/itr_90.pt"
+            state_dict = torch.load(snapshot_path, map_location=get_device())
+            policy.load_state_dict(state_dict)
 
-                shot_selector = shot_selector_func(trainset, args.shot_num, resolver=resolver, policy=policy, env=env)
-                val_acc = test(language_model, tokenizer, val_dataloader, shot_selector, truncator)
-                test_acc = test(language_model, tokenizer, test_dataloader, shot_selector, truncator)
-                print(f"[{seed}/4] Val Acc {val_acc * 100:.1f} Test Accuracy {test_acc * 100:.1f}")
-                del shot_selector
+            shot_selector = shot_selector_func(trainset, args.shot_num, resolver=resolver, policy=policy, env=env)
+            val_acc = test(language_model, tokenizer, val_dataloader, shot_selector, truncator)
+            test_acc = test(language_model, tokenizer, test_dataloader, shot_selector, truncator)
+            print(f"[{seed}/4] Val Acc {val_acc * 100:.1f} Test Accuracy {test_acc * 100:.1f}")
+            del shot_selector
 
-        elif shot_selector_func == shot_selectors.random:
-            for repeat in range(20):
-                shot_selector = shot_selector_func(trainset, args.shot_num, resolver=resolver)
-                val_acc = test(language_model, tokenizer, val_dataloader, shot_selector, truncator)
-                test_acc = test(language_model, tokenizer, test_dataloader, shot_selector, truncator)
-                print(f"[{repeat}/20] Val Acc {val_acc * 100:.1f} Test Accuracy {test_acc * 100:.1f}")
-                del shot_selector
-
-        elif shot_selector_func == shot_selectors.closest:
+    elif shot_selector_func == shot_selectors.random:
+        for repeat in range(4):
             shot_selector = shot_selector_func(trainset, args.shot_num, resolver=resolver)
             val_acc = test(language_model, tokenizer, val_dataloader, shot_selector, truncator)
             test_acc = test(language_model, tokenizer, test_dataloader, shot_selector, truncator)
-            print(f"[0/1] Val Acc {val_acc * 100:.1f} Test Accuracy {test_acc * 100:.1f}")
+            print(f"[{repeat}/4] Val Acc {val_acc * 100:.1f} Test Accuracy {test_acc * 100:.1f}")
             del shot_selector
+
+    elif shot_selector_func == shot_selectors.closest:
+        shot_selector = shot_selector_func(trainset, args.shot_num, resolver=resolver)
+        val_acc = test(language_model, tokenizer, val_dataloader, shot_selector, truncator)
+        test_acc = test(language_model, tokenizer, test_dataloader, shot_selector, truncator)
+        print(f"[0/1] Val Acc {val_acc * 100:.1f} Test Accuracy {test_acc * 100:.1f}")
+        del shot_selector
 
 
 if __name__ == '__main__':
@@ -141,6 +140,7 @@ if __name__ == '__main__':
 
     parser.add_argument('--batch_size', type=int, default=4)
     parser.add_argument('--shot_num', type=int, default=2)
+    parser.add_argument('--shot_selector', type=str, default='ours')
     args = parser.parse_args()
 
     main(args)
